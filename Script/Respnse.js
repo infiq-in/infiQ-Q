ab573cbf1e8016143c793f2f8dc2bd7906b16bf5
infiQ <141230405+infiq-in@users.noreply.github.com>
2023-08-22 23:03:07 +0530

    Add files via upload

---
+++ Script/Respnse.js
@@ -0,0 +1,17 @@
+function sendEmail(){
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let email = document.getElementById("email").value;
+    let number = document.getElementById("Cnum").value;
+    let message = document.getElementById("msg").value;
+    let body = "Name : " + fname+" "+lname +"<br>Email : "+email+"<br>Contact Number : "+number+"<br> Message : "+message; 
+    Email.send({
+    SecureToken :  "fe35663c-638e-4fd9-88f5-64b22f43e6a6",
+    To : 'infiqteam@gmail.com',
+    From : 'infiqteam@gmail.com',
+    Subject : "Contact Form Response",
+    Body : body
+}).then(
+  message => alert(message)
+);
+    }
